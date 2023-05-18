6f6a61dd55b0bba904b82dad87e43c45c7cd481a
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-05-18 21:41:32 +0300

    Flask added. Code will be improved.

--- decision_tree.py
+++ decision_tree.py
@@ -3,107 +3,128 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
+from flask import Flask, request, jsonify
 
-# Relative file path for dataset
-file_path = './assets/data/Triyaj.xls'
-
-# Read the XLS file
-data_set = pd.read_excel(file_path, sheet_name=3)
-
-# multiply data
-copies = 25
-rows_to_copy = [data_set] * copies
-data_set = pd.concat(rows_to_copy, ignore_index=True)
-
-# Convert "BELİRTİ AD" column to categorical values using LabelEncoder
-label_encoder = LabelEncoder()
-label_values = data_set['BELİRTİ AD'].unique()
-label_encoder.fit(label_values)
-data_set["BELİRTİ_SAYISAL"] = label_encoder.transform(data_set["BELİRTİ AD"])
-
-# Remove rows with 0.0 in a specific column
-data_set = data_set[data_set['GÖZLENME SIKLIĞI'] > 0.1]
-
-# Add new columns to increase diversity
-data_set['BELİRTİ_GÖZLENME'] = data_set['BELİRTİ_SAYISAL'] * \
-    data_set['GÖZLENME SIKLIĞI']
+app = Flask(__name__)
 
 
-data_set['BELİRTİ_GÖZLENME1'] = data_set['BELİRTİ_SAYISAL'] / \
-    data_set['GÖZLENME SIKLIĞI']
+@app.route('/diagnosis', methods=['GET'])
+def diagnosis():
 
+    # Relative file path for dataset
+    file_path = './assets/data/Triyaj.xls'
 
-# Decision tree algorithm implementation
-# Separating data
-X = data_set.iloc[:, 2:7]  # input values
-Y = data_set.iloc[:, 0]  # output, target values
+    # Read the XLS file
+    data_set = pd.read_excel(file_path, sheet_name=3)
 
-# Split data as train and test
-# Test data will be 30% percent of the data and it will be selected randomly
-X_train, X_test, Y_train, Y_test = train_test_split(
-    X, Y, test_size=0.30)
+    # multiply data
+    copies = 25
+    rows_to_copy = [data_set] * copies
+    data_set = pd.concat(rows_to_copy, ignore_index=True)
 
-# Create a decision tree classifier
-clf_entropy = DecisionTreeClassifier(
-    criterion="entropy", max_depth=12, min_samples_leaf=10)
+    # Convert "BELİRTİ AD" column to categorical values using LabelEncoder
+    label_encoder = LabelEncoder()
+    label_values = data_set['BELİRTİ AD'].unique()
+    label_encoder.fit(label_values)
+    data_set["BELİRTİ_SAYISAL"] = label_encoder.transform(
+        data_set["BELİRTİ AD"])
 
-# Train the decision tree model on the provided training data
-clf_entropy.fit(X_train, Y_train)
+    # Remove rows with 0.0 in a specific column
+    data_set = data_set[data_set['GÖZLENME SIKLIĞI'] > 0.1]
 
-# Make prediction
-Y_prediction = clf_entropy.predict(X_test)
+    # Add new columns to increase diversity
+    data_set['BELİRTİ_GÖZLENME'] = data_set['BELİRTİ_SAYISAL'] * \
+        data_set['GÖZLENME SIKLIĞI']
 
-# Display prediction accuracy
-ac_score = accuracy_score(Y_test, Y_prediction) * 100
-# print("Accuracy of prediction is : ", ac_score)
+    data_set['BELİRTİ_GÖZLENME1'] = data_set['BELİRTİ_SAYISAL'] / \
+        data_set['GÖZLENME SIKLIĞI']
 
-# Get user symptom and create dataframe as similar to used in test and train
-multiple_predictons = list()
-user_symptom = "baş ağrısı,ateş,bel ağrısı"
-user_symptoms = user_symptom.split(",")
+    # Decision tree algorithm implementation
+    # Separating data
+    X = data_set.iloc[:, 2:7]  # input values
+    Y = data_set.iloc[:, 0]  # output, target values
 
-for symptom in user_symptoms:
-    user_predictions_list = list()
-    user_symptom_int = data_set.loc[data_set['BELİRTİ AD']
-                                    == symptom, 'BELİRTİ_SAYISAL'].iloc[0]
+    # Split data as train and test
+    # Test data will be 30% percent of the data and it will be selected randomly
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=0.30)
 
-    for i in range(7, 10):
-        user_symptom_df = {'GÖZLENME SIKLIĞI': [(i / 10)],
-                           'BELİRTİ_SAYISAL': [user_symptom_int],
-                           'BELİRTİ_GÖZLENME': [((i / 10) * user_symptom_int)],
-                           'BELİRTİ_GÖZLENME1': [((i / 10) + user_symptom_int)]
-                           }
+    # Create a decision tree classifier
+    clf_entropy = DecisionTreeClassifier(
+        criterion="entropy", max_depth=12, min_samples_leaf=10)
 
-        user_symptom_df = pd.DataFrame(user_symptom_df)
+    # Train the decision tree model on the provided training data
+    clf_entropy.fit(X_train, Y_train)
 
-        # Make prediction for user
-        user_prediction = clf_entropy.predict(user_symptom_df)
-        user_predictions_list.append(user_prediction[0])
+    # Make prediction
+    Y_prediction = clf_entropy.predict(X_test)
 
-    multiple_predictons.append(user_predictions_list)
+    # Display prediction accuracy
+    ac_score = accuracy_score(Y_test, Y_prediction) * 100
+    # print("Accuracy of prediction is : ", ac_score)
 
+    # Get user symptom and create dataframe as similar to used in test and train
+    multiple_predictons = list()
 
-# get common disease by merging
-merged_list = [item for sublist in multiple_predictons for item in sublist]
+    user_symptom = request.args.get('symptom')
+    # user_symptom = "baş ağrısı,ateş,bel ağrısı"
+    try:
+        user_symptoms = user_symptom.split(",")
+    except AttributeError:
+        return jsonify({'success': 'false', 'error': 'No sympton.'})
 
-# Create a dictionary to store the frequency of elements
-frequency_dict = {}
-for item in merged_list:
-    if item in frequency_dict:
-        frequency_dict[item] += 1
+    for symptom in user_symptoms:
+        user_predictions_list = list()
+        try:
+            user_symptom_int = data_set.loc[data_set['BELİRTİ AD']
+                                            == symptom, 'BELİRTİ_SAYISAL'].iloc[0]
+        except IndexError:
+            return jsonify({'success': 'false', 'error': 'Enter valid sympton.'})
+
+        for i in range(7, 10):
+            user_symptom_df = {'GÖZLENME SIKLIĞI': [(i / 10)],
+                               'BELİRTİ_SAYISAL': [user_symptom_int],
+                               'BELİRTİ_GÖZLENME': [((i / 10) * user_symptom_int)],
+                               'BELİRTİ_GÖZLENME1': [((i / 10) + user_symptom_int)]
+                               }
+
+            user_symptom_df = pd.DataFrame(user_symptom_df)
+
+            # Make prediction for user
+            user_prediction = clf_entropy.predict(user_symptom_df)
+            user_predictions_list.append(user_prediction[0])
+
+        multiple_predictons.append(user_predictions_list)
+
+    # get common disease by merging
+    merged_list = [item for sublist in multiple_predictons for item in sublist]
+
+    # Create a dictionary to store the frequency of elements
+    frequency_dict = {}
+    for item in merged_list:
+        if item in frequency_dict:
+            frequency_dict[item] += 1
+        else:
+            frequency_dict[item] = 1
+
+    # Sort frequency dictionary
+    sorted_freq = sorted(frequency_dict.items(),
+                         key=lambda x: x[1], reverse=True)
+
+    # If the number of results is more than expected, get first 3
+    if len(sorted_freq) > 3:
+        result = [item[0] for item in sorted_freq[:3]]
     else:
-        frequency_dict[item] = 1
+        result = [item[0] for item in sorted_freq]
 
-# sort frequency dictionary
-sorted_freq = sorted(frequency_dict.items(), key=lambda x: x[1], reverse=True)
+    # Display result to user
+    # print("\n----")
+    # print("Olası hastalık tanıları : ", result)
+    # print("----\n")
 
-# if the number of results is more than expected
-if len(sorted_freq) > 3:
-    result = [item[0] for item in sorted_freq[:3]]
-else:
-    result = [item[0] for item in sorted_freq]
+    # Return result as json
+    return jsonify({'success': 'true', 'result': result})
 
-# display result to user
-print("\n----")
-print("Olası hastalık tanıları : ", result)
-print("----\n")
+
+if __name__ == '__main__':
+    app.run(debug=True)
